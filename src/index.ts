f821efd3f05fce7c2e5a596c274db5c0c8454bab
DrRawley <rawley.greene@gmail.com>
2024-01-05 15:19:08 -0500

    updated src structuure and package.json file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 //All the interface definintion exports
-export { IAuthPayload, IAuth, IAuthDocument, IAuthBuyerMessageDetails, IEmailMessageDetails, ISignUpPayload, ISignInPayload, IForgotPassword, IResetPassword, IReduxAuthPayload, IReduxAddAuthUser, IReduxLogout, IAuthResponse, IAuthUser } from './interfaces/auth.interface';
-export { IBuyerDocument, IReduxBuyer } from './interfaces/buyer.interface';
-export { IConversationDocument, IMessageDocument, IMessageDetails, IChatBoxProps, IChatSellerProps, IChatBuyerProps, IChatMessageProps } from './interfaces/chat.interface';
-export { IEmailLocals } from './interfaces/email.interface';
-export { GigType, ICreateGig, ISellerGig, IGigContext, IGigsProps, IGigCardItems, ISelectedBudget, IGigViewReviewsProps, IGigInfo, IGigTopProps } from './interfaces/gig.interface';
-export { IOffer, IExtendedDelivery, IDeliveredWork, IOrderEvents, IOrderReview, IOrderMessage, IOrderDocument, IOrderNotifcation } from './interfaces/order.interface';
-export { IReviewMessageDetails, IRatingTypes, IReviewDocument, IRatingCategoryItem, IRatingCategories } from './interfaces/review.interface';
-export { ISearchResult, IHitsTotal, IQueryList, IQueryString, ITerm, IPaginateProps } from './interfaces/search.interface';
-export { SellerType, ILanguage, IExperience, IEducation, ICertificate, ISellerDocument } from './interfaces/seller.interface';
+export { IAuthPayload, IAuth, IAuthDocument, IAuthBuyerMessageDetails, IEmailMessageDetails, ISignUpPayload, ISignInPayload, IForgotPassword, IResetPassword, IReduxAuthPayload, IReduxAddAuthUser, IReduxLogout, IAuthResponse, IAuthUser } from './auth.interface';
+export { IBuyerDocument, IReduxBuyer } from './buyer.interface';
+export { IConversationDocument, IMessageDocument, IMessageDetails, IChatBoxProps, IChatSellerProps, IChatBuyerProps, IChatMessageProps } from './chat.interface';
+export { IEmailLocals } from './email.interface';
+export { GigType, ICreateGig, ISellerGig, IGigContext, IGigsProps, IGigCardItems, ISelectedBudget, IGigViewReviewsProps, IGigInfo, IGigTopProps } from './gig.interface';
+export { IOffer, IExtendedDelivery, IDeliveredWork, IOrderEvents, IOrderReview, IOrderMessage, IOrderDocument, IOrderNotifcation } from './order.interface';
+export { IReviewMessageDetails, IRatingTypes, IReviewDocument, IRatingCategoryItem, IRatingCategories } from './review.interface';
+export { ISearchResult, IHitsTotal, IQueryList, IQueryString, ITerm, IPaginateProps } from './search.interface';
+export { SellerType, ILanguage, IExperience, IEducation, ICertificate, ISellerDocument } from './seller.interface';
 //Cloudinaryr exports
 export { uploads, videoUpload } from './cloudinary-upload';
 //Error handling exports
