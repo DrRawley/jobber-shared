bca7d31a054a93eee7d308c1219bff5a9d33062f
DrRawley <rawley.greene@gmail.com>
2024-01-05 13:03:08 -0500

    add misc helpers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+//All the interface definintion exports
 export { IAuthPayload, IAuth, IAuthDocument, IAuthBuyerMessageDetails, IEmailMessageDetails, ISignUpPayload, ISignInPayload, IForgotPassword, IResetPassword, IReduxAuthPayload, IReduxAddAuthUser, IReduxLogout, IAuthResponse, IAuthUser } from './interfaces/auth.interface';
 export { IBuyerDocument, IReduxBuyer } from './interfaces/buyer.interface';
 export { IConversationDocument, IMessageDocument, IMessageDetails, IChatBoxProps, IChatSellerProps, IChatBuyerProps, IChatMessageProps } from './interfaces/chat.interface';
@@ -7,11 +8,13 @@ export { IOffer, IExtendedDelivery, IDeliveredWork, IOrderEvents, IOrderReview, 
 export { IReviewMessageDetails, IRatingTypes, IReviewDocument, IRatingCategoryItem, IRatingCategories } from './interfaces/review.interface';
 export { ISearchResult, IHitsTotal, IQueryList, IQueryString, ITerm, IPaginateProps } from './interfaces/search.interface';
 export { SellerType, ILanguage, IExperience, IEducation, ICertificate, ISellerDocument } from './interfaces/seller.interface';
-
+//Cloudinaryr exports
 export { uploads, videoUpload } from './cloudinary-upload';
-
+//Error handling exports
 export { BadRequestError, CustomError, ErrnoException, FileTooLargeError, IError, IErrorResponse, NotAuthorizedError, NotFoundError, ServerError } from './error-handler';
-
+//verify gateway source exports
 export { verifyGatewayRequeset } from './gateway-middleware';
-
+//Logger handler export
 export { winstonLogger } from './logger';
+//Various helpers exports
+export { firstLetterUppercase, isDataURL, isEmail, lowerCase, toUpperCase } from './helpers';
